d4e33ba79c5cc849a0c9e31562b87054c34c23e9
malikeshz <ibrahimabdulmalik707@gmail.com>
2024-03-18 10:29:59 +0100

    menu icon for mobile

--- scripts/script.js
+++ scripts/script.js
@@ -17,3 +17,12 @@ var swiper = new Swiper(".slide-content", {
       prevEl: ".swiper-button-prev",
     },
   });
+
+  function showSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
+  }
+  function hideSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+  }
